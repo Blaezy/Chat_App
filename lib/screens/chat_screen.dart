5764c7e9a373884a0ae12ae3a03e91298e709f48
Shivam Kumar <iamrealshivamskyler@gmail.com>
2020-09-14 21:46:22 +0530

    Final Commit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -2,10 +2,34 @@ import 'package:chat_app/widgets/chat/messages.dart';
 import 'package:chat_app/widgets/chat/newmessage.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-class ChatScreen extends StatelessWidget {
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  void initState() {
+    //only Needed For Ios Devices No need in Android Devices
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(onLaunch: (msg) {
+      print(msg);
+      return;
+    }, onMessage: (msg) {
+      print(msg);
+      return;
+    }, onResume: (msg) {
+      print(msg);
+      return;
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     print('hello');
